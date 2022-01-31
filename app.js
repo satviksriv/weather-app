70519c7fcfaa9536098828bbcb35ac56cd1047e8
satviksriv <srivastavasatvik22@gmail.com>
2022-01-31 20:48:20 +0530

    Explored different fields provided by the weather api

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require("request");
 
-const url = "http://api.weatherstack.com/current?access_key=54eb38aaee318bb2f6f8e66833c5f35c&query=37.8267,-122.4233";
+const url = "http://api.weatherstack.com/current?access_key=54eb38aaee318bb2f6f8e66833c5f35c&query=37.8267,-122.4233&units=f";
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.current);
+    console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`);
 })
